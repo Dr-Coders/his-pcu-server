c6b1b1eecaa04c3168e189e4325f4fd56e66ac10
NirmalAriyathilake <nirmal.ariyathilake@my.sliit.lk>
2017-06-29 00:56:39 +0530

    PCU Server

---
+++ app/models/user.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Nirmal on 6/29/2017.
+ */
+
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+    username : String,
+    password : String,
+    objectid : String
+
+
+}, { collection: 'user' });
+
+module.exports = mongoose.model("User",UserSchema);
