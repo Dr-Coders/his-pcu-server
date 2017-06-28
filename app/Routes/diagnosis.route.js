404d36c0ebf53f4ea2461c3e8e1512737f0624c9
NirmalAriyathilake <nirmal.ariyathilake@my.sliit.lk>
2017-06-28 17:06:35 +0530

    PCU Server

--- app/Routes/diagnosis.route.js
+++ app/Routes/diagnosis.route.js
@@ -6,58 +6,68 @@ const express = require("express");
 const Diagnosis = require("../models/diagnosis");
 const router = express.Router();
 
-router.use(function (req,res,next) {
+router.use(function (req, res, next) {
     console.log("Accessing Diagnosis API");
     next();
 });
 
 router.route("/")
-    .post(function (req,res) {
+    .post(function (req, res) {
         var diagnosis = new Diagnosis(req.body);
 
         diagnosis.save(function (err) {
-            if(err)
+            if (err)
                 res.send(err);
 
             res.json({message: "Diagnosis created! name = " + req.body.title});
         });
     })
-    .get(function (req,res) {
-        Diagnosis.find(function (err,diagnosis) {
-            if(err)
-                res.send(err);
-            console.log("Diagnosis : " + diagnosis);
-            res.json(diagnosis);
-        })
+    .get(function (req, res) {
+        // Diagnosis.find(function (err, diagnosis) {
+        //     if (err)
+        //         res.send(err);
+        //     console.log("Diagnosis : " + diagnosis);
+        //     res.json(diagnosis);
+        // });
+        Diagnosis
+            .find()
+            .populate('doctor')
+            .populate('patient')// only return the Bears name
+            .exec(function (err, diagnosis) {
+                if (err)
+                    res.send(err);
+                console.log("Data : " + diagnosis[0].doctor.firstname);
+                res.json(diagnosis);
+            })
     });
 
 router.route("/:id")
-    .get(function (req,res) {
-        Diagnosis.findById(req.params.id,function (err,diagnosis) {
-            if(err)
+    .get(function (req, res) {
+        Diagnosis.findById(req.params.id, function (err, diagnosis) {
+            if (err)
                 res.send(err);
             res.json(diagnosis);
         });
     })
-    .put(function (req,res) {
-        Diagnosis.findById(req.params.id,function (err,diagnosis) {
-            if(err)
+    .put(function (req, res) {
+        Diagnosis.findById(req.params.id, function (err, diagnosis) {
+            if (err)
                 res.send(err);
             diagnosis = req.body;
 
             console.log("updating... " + req.params.id);
             diagnosis.save(function (err) {
-                if(err)
+                if (err)
                     res.send(err);
-                res.json({message : " Diagnosis updated to name = " + req.body.title});
+                res.json({message: " Diagnosis updated to name = " + req.body.title});
             })
         })
     })
-    .delete(function (req,res) {
+    .delete(function (req, res) {
         Diagnosis.remove({
-            _id:req.params.id
-        },function (err) {
-            if(err)
+            _id: req.params.id
+        }, function (err) {
+            if (err)
                 res.send(err);
             res.json({message: "Diagnosis deleted : " + req.params.id})
         });
